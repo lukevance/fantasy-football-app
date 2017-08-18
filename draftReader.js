f47207afd5890248251a022ea4d8f788664c8c2d
Luke Vance <lukevance90@gmail.com>
2017-08-17 23:23:44 -0600

    updated html parser module

--- draftReader.js
+++ draftReader.js
@@ -4,9 +4,30 @@ var fs = require('fs')
 var htmlString = fs.readFileSync('./data/draftRecap.html').toString()
 var parsedHTML = $.load(htmlString)
 
-// query for all elements with class 'foo' and loop over them
-parsedHTML('table').map(function(i, table) {
-    // the foo html element into a cheerio object (same pattern as jQuery)
-    table = $(table)
-    console.log(table.children('td'))
-})
+// query
+// for all elements type 'table'
+// and loop over them
+// parsedHTML('table').map((i, table) => {
+//     if (i = 1) {
+//         // the table html element into a cheerio object
+//         table = $(table)
+//         console.log(table.children('td'))
+//         console.log('-------------------------');
+//         console.log(i);
+//     }
+// });
+
+let rounds = parsedHTML('table');
+// for (var key in firstTable) {
+//     console.log(key);
+// }
+
+
+console.log(rounds.children());
+console.log(rounds.length); // 16
+console.log('----------');
+// console.log(rounds["0"].children["1"].children["0"].data); // === ROUND 1!!!
+console.log(rounds["0"].children["1"].children["1"].children["1"].children["0"].data);
+// let newTable = Object.keys(firstTable).filter(child => child !== 'options');
+
+// console.log(Object.keys(newTable));
