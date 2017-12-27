f25d73fc978c483a5219712c0cdc1e4586d87f6d
Luke Vance <lukevance90@gmail.com>
2017-12-26 22:23:19 -0700

    added menu and table

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import Button from "material-ui/Button";
+import NavBar from "./NavBar";
+import ScoreTable from "./ScoreTable";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <NavBar />
+        <ScoreTable 
+            style={{
+                padding: "20px"
+            }}
+        />
+        <Button 
+            raised 
+            color="primary"
+            style={{
+                marginTop: "20px"
+            }}
+        >
+            Hello World
+        </Button>
       </div>
     );
   }
