67410e9d2e4ee23c878964e69e91d048d6cd16e3
Luke Vance <luke@cloud-elements.com>
2018-04-28 22:52:01 -0600

    updated tests sample for reading data

--- tests/team_from_scoreboard.js
+++ tests/team_from_scoreboard.js
@@ -1,3 +1,15 @@
-const scoreboardData = require('./scoreboard_luke_wk15.json');
+// const scoreboardData = require('./scoreboard_luke_wk15.json');
 
-scoreboardData.teams.forEach(x => console.log(Object.keys(x)));
+// scoreboardData.teams.forEach(x => console.log(Object.keys(x)));
+
+const scoreBoard = require("../src/espnReader/scoreboard");
+// API handler to get team score data for current team in row
+getTeamScore = async (week, teamId, leagueId) => {
+    // let { leagueId, week } = this.props;
+    let scoreboardData = await scoreBoard(leagueId, teamId, week);
+    // if (teamId === 7) {console.log(scoreboardData)};
+    return await scoreboardData;
+};
+
+
+console.log(getTeamScore(15, 7, 286565));
