cda5ca3389429f6518a51b12cc79f95fb7af24ce
Luke Vance <luke@cloud-elements.com>
2018-08-15 10:03:13 -0400

    moved mock function for easy dev

---
+++ src/espnReader/mock-func-my-team-season-recap.js
@@ -0,0 +1,622 @@
+module.exports = {
+    'QB': [{
+            name: 'Tyrod Taylor',
+            week: 1,
+            score: 22.76,
+            active: true
+        },
+        {
+            name: 'Tyrod Taylor',
+            games: {
+                week: 2,
+                score: 10.5
+            }
+        },
+        {
+            name: 'Philip Rivers',
+            games: {
+                week: 3,
+                score: 3.48
+            }
+        },
+        {
+            name: 'Tyrod Taylor',
+            games: {
+                week: 4,
+                score: 14.48
+            }
+        },
+        {
+            name: 'Deshaun Watson',
+            games: {
+                week: 5,
+                score: 45.54
+            }
+        },
+        {
+            name: 'Deshaun Watson',
+            games: {
+                week: 6,
+                score: 29.3
+            }
+        },
+        {
+            name: 'Jared Goff',
+            games: {
+                week: 7,
+                score: 20.7
+            }
+        },
+        {
+            name: 'Deshaun Watson',
+            games: {
+                week: 8,
+                score: 40.78
+            }
+        },
+        {
+            name: 'Jared Goff',
+            games: {
+                week: 9,
+                score: 36.44
+            }
+        },
+        {
+            name: 'Jared Goff',
+            games: {
+                week: 10,
+                score: 32.5
+            }
+        },
+        {
+            name: 'Jay Cutler',
+            games: {
+                week: 11,
+                score: 3.42
+            }
+        },
+        {
+            name: 'Jared Goff',
+            games: {
+                week: 12,
+                score: 23.86
+            }
+        },
+        {
+            name: 'Philip Rivers',
+            games: {
+                week: 13,
+                score: 19.56
+            }
+        }
+    ],
+    'RB': [
+        [{
+                name: 'Bilal Powell',
+                week: 1,
+                score: 5.9,
+                active: true
+            },
+            {
+                name: 'Melvin Gordon',
+                week: 1,
+                score: 15.9,
+                active: true
+            },
+            {
+                name: 'Ty Montgomery',
+                week: 1,
+                score: 16.9,
+                active: false
+            },
+            {
+                name: 'Thomas Rawls',
+                week: 1,
+                score: 0,
+                active: false
+            },
+            {
+                name: 'James White',
+                week: 1,
+                score: 8,
+                active: false
+            },
+            {
+                name: 'Jonathan Stewart',
+                week: 1,
+                score: 15,
+                active: false
+            },
+            {
+                name: 'Giovani Bernard',
+                week: 1,
+                score: 8.3,
+                active: false
+            }
+        ],
+        [{
+                name: 'Ty Montgomery',
+                week: 2,
+                score: 25.4,
+                active: true
+            },
+            {
+                name: 'Melvin Gordon',
+                week: 2,
+                score: 16.6,
+                active: true
+            },
+            {
+                name: 'James White',
+                week: 2,
+                score: 12.8,
+                active: false
+            },
+            {
+                name: 'Bilal Powell',
+                week: 2,
+                score: 1.3,
+                active: false
+            },
+            {
+                name: 'Thomas Rawls',
+                week: 2,
+                score: 0.4,
+                active: false
+            },
+            {
+                name: 'Jonathan Stewart',
+                week: 2,
+                score: 4,
+                active: false
+            },
+            {
+                name: 'Giovani Bernard',
+                week: 2,
+                score: 3.4,
+                active: false
+            }
+        ],
+        [{
+                name: 'Ty Montgomery',
+                week: 3,
+                score: 8.2,
+                active: true
+            },
+            {
+                name: 'Melvin Gordon',
+                week: 3,
+                score: 13.9,
+                active: true
+            },
+            {
+                name: 'James White',
+                week: 3,
+                score: 3.2,
+                active: false
+            },
+            {
+                name: 'Bilal Powell',
+                week: 3,
+                score: 9.7,
+                active: false
+            },
+            {
+                name: 'Thomas Rawls',
+                week: 3,
+                score: 0,
+                active: false
+            },
+            {
+                name: 'Jonathan Stewart',
+                week: 3,
+                score: 6.5,
+                active: false
+            }
+        ],
+        [{
+                name: 'Ty Montgomery',
+                week: 4,
+                score: 2.8,
+                active: true
+            },
+            {
+                name: 'Melvin Gordon',
+                week: 4,
+                score: 3.3,
+                active: true
+            },
+            {
+                name: 'Bilal Powell',
+                week: 4,
+                score: 26.6,
+                active: false
+            },
+            {
+                name: 'Thomas Rawls',
+                week: 4,
+                score: 0,
+                active: false
+            },
+            {
+                name: 'James White',
+                week: 4,
+                score: 9.4,
+                active: false
+            },
+            {
+                name: 'Jonathan Stewart',
+                week: 4,
+                score: 4.8,
+                active: false
+            }
+        ],
+        [{
+                name: 'Melvin Gordon',
+                week: 5,
+                score: 30.7,
+                active: true
+            },
+            {
+                name: 'James White',
+                week: 5,
+                score: 8.8,
+                active: true
+            },
+            {
+                name: 'Bilal Powell',
+                week: 5,
+                score: 4.9,
+                active: false
+            },
+            {
+                name: 'Ty Montgomery',
+                week: 5,
+                score: 0,
+                active: false
+            },
+            {
+                name: 'Jonathan Stewart',
+                week: 5,
+                score: 5.6,
+                active: false
+            },
+            {
+                name: 'Eddie Lacy',
+                week: 5,
+                score: 3.2,
+                active: false
+            }
+        ],
+        [{
+                name: 'Ty Montgomery',
+                week: 6,
+                score: 3.5,
+                active: true
+            },
+            {
+                name: 'Melvin Gordon',
+                week: 6,
+                score: 30.6,
+                active: true
+            },
+            {
+                name: 'Bilal Powell',
+                week: 6,
+                score: 0,
+                active: false
+            },
+            {
+                name: 'James White',
+                week: 6,
+                score: 6.1,
+                active: false
+            },
+            {
+                name: 'Jonathan Stewart',
+                week: 6,
+                score: -0.4,
+                active: false
+            },
+            {
+                name: 'Matt Breida',
+                week: 6,
+                score: 4.4,
+                active: false
+            }
+        ],
+        [{
+                name: 'Ty Montgomery',
+                week: 7,
+                score: 1.9,
+                active: true
+            },
+            {
+                name: 'Melvin Gordon',
+                week: 7,
+                score: 4.8,
+                active: true
+            },
+            {
+                name: 'Dion Lewis',
+                week: 7,
+                score: 8.6,
+                active: false
+            },
+            {
+                name: 'Bilal Powell',
+                week: 7,
+                score: 9.1,
+                active: false
+            },
+            {
+                name: 'James White',
+                week: 7,
+                score: 12.7,
+                active: false
+            },
+            {
+                name: 'Matt Breida',
+                week: 7,
+                score: 0.5,
+                active: false
+            }
+        ],
+        [{
+                name: 'Melvin Gordon',
+                week: 8,
+                score: 20.3,
+                active: true
+            },
+            {
+                name: 'James White',
+                week: 8,
+                score: 10.7,
+                active: true
+            },
+            {
+                name: 'Ty Montgomery',
+                week: 8,
+                score: undefined,
+                active: false
+            },
+            {
+                name: 'Bilal Powell',
+                week: 8,
+                score: 7.3,
+                active: false
+            },
+            {
+                name: 'Dion Lewis',
+                week: 8,
+                score: 6.2,
+                active: false
+            },
+            {
+                name: 'Austin Ekeler',
+                week: 8,
+                score: 2.5,
+                active: false
+            }
+        ],
+        [{
+                name: 'Bilal Powell',
+                week: 9,
+                score: 7.4,
+                active: true
+            },
+            {
+                name: 'Aaron Jones',
+                week: 9,
+                score: 1.9,
+                active: true
+            },
+            {
+                name: 'Theo Riddick',
+                week: 9,
+                score: 8.9,
+                active: false
+            },
+            {
+                name: 'Ty Montgomery',
+                week: 9,
+                score: 5.5,
+                active: false
+            },
+            {
+                name: 'Melvin Gordon',
+                week: 9,
+                score: undefined,
+                active: false
+            },
+            {
+                name: 'James White',
+                week: 9,
+                score: undefined,
+                active: false
+            },
+            {
+                name: 'Austin Ekeler',
+                week: 9,
+                score: undefined,
+                active: false
+            }
+        ],
+        [{
+                name: 'Bilal Powell',
+                week: 10,
+                score: 2,
+                active: true
+            },
+            {
+                name: 'Melvin Gordon',
+                week: 10,
+                score: 6.2,
+                active: true
+            },
+            {
+                name: 'James White',
+                week: 10,
+                score: 9,
+                active: false
+            },
+            {
+                name: 'Ty Montgomery',
+                week: 10,
+                score: 13.6,
+                active: false
+            },
+            {
+                name: 'Austin Ekeler',
+                week: 10,
+                score: 23.9,
+                active: false
+            },
+            {
+                name: 'Aaron Jones',
+                week: 10,
+                score: 1.6,
+                active: false
+            },
+            {
+                name: 'Danny Woodhead',
+                week: 10,
+                score: undefined,
+                active: false
+            }
+        ],
+        [{
+                name: 'Melvin Gordon',
+                week: 11,
+                score: 14,
+                active: true
+            },
+            {
+                name: 'Jamaal Williams',
+                week: 11,
+                score: 11.1,
+                active: true
+            },
+            {
+                name: 'James White',
+                week: 11,
+                score: 1.3,
+                active: false
+            },
+            {
+                name: 'Ty Montgomery',
+                week: 11,
+                score: 0,
+                active: false
+            },
+            {
+                name: 'Bilal Powell',
+                week: 11,
+                score: undefined,
+                active: false
+            },
+            {
+                name: 'Austin Ekeler',
+                week: 11,
+                score: 12.6,
+                active: false
+            },
+            {
+                name: 'Danny Woodhead',
+                week: 11,
+                score: 4.3,
+                active: false
+            }
+        ],
+        [{
+                name: 'Melvin Gordon',
+                week: 12,
+                score: 10,
+                active: true
+            },
+            {
+                name: 'Jamaal Williams',
+                week: 12,
+                score: 27.1,
+                active: true
+            },
+            {
+                name: 'Danny Woodhead',
+                week: 12,
+                score: 6.1,
+                active: false
+            },
+            {
+                name: 'Ty Montgomery',
+                week: 12,
+                score: 0,
+                active: false
+            },
+            {
+                name: 'Bilal Powell',
+                week: 12,
+                score: 5,
+                active: false
+            },
+            {
+                name: 'James White',
+                week: 12,
+                score: 1.9,
+                active: false
+            },
+            {
+                name: 'Austin Ekeler',
+                week: 12,
+                score: 8.2,
+                active: false
+            }
+        ],
+        [{
+                name: 'Melvin Gordon',
+                week: 13,
+                score: 12.2,
+                active: true
+            },
+            {
+                name: 'Jamaal Williams',
+                week: 13,
+                score: 19.1,
+                active: true
+            },
+            {
+                name: 'Danny Woodhead',
+                week: 13,
+                score: 3.1,
+                active: false
+            },
+            {
+                name: 'Ty Montgomery',
+                week: 13,
+                score: 0,
+                active: false
+            },
+            {
+                name: 'Bilal Powell',
+                week: 13,
+                score: 11.8,
+                active: false
+            },
+            {
+                name: 'Austin Ekeler',
+                week: 13,
+                score: 4.8,
+                active: false
+            },
+            {
+                name: 'Peyton Barber',
+                week: 13,
+                score: 15.9,
+                active: false
+            }
+        ]
+    ],
+    'WR': [],
+    'TE': [],
+    'D': [],
+    'K': []
+};
